7d4d7b953a9a1b5ec014302ffc35c600194efa29
Sosai <48029592+ItMeansBigMountain@users.noreply.github.com>
2026-09-05 05:49:24 +0000

    fix: consolidate Terraform state into CWB resource group

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-cwb-tfstate"
+    resource_group_name  = "rg-cwb-dev"
     storage_account_name = "cwbtfstate4f070006f5"
     container_name       = "tfstate"
     key                  = "trap-chat/prod.tfstate"
